c260e41d7c4c47bb1372b88c1a12c97db73532da
toreyjh <121916380+toreyjh@users.noreply.github.com>
2023-01-12 19:49:23 -0500

    Update Torey.java

--- GitHubPushPull/src/teamMembers/Torey.java
+++ GitHubPushPull/src/teamMembers/Torey.java
@@ -3,6 +3,8 @@ package teamMembers;
 public class Torey {
 	public static void main(String[] args) {
 		System.out.println("Torey");
+			System.out.println("This edit was done on GitHut by Torey");
+		
 	}
 
 }
