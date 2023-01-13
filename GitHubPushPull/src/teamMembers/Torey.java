198e94b195532b32522d933c96e1a72d875a7018
Torey <Torey@TJH-DI5485>
2023-01-12 20:54:45 -0500

    updating

--- GitHubPushPull/src/teamMembers/Torey.java
+++ GitHubPushPull/src/teamMembers/Torey.java
@@ -5,11 +5,6 @@ public class Torey {
 		System.out.println("Torey");
 			System.out.println("This edit was done on GitHut by Torey");
 		
-		System.out.println("Updated on January 12 at 8:02PMy");
-		
-		System.out.println("Updated from Eclipse at 839pm");
-		
-		
 	}
 
 }
